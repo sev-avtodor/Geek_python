54fcd37a0b6181135cf3fa7be86dd4973c45d47a
Artem <molodov@sevavtodor.ru>
2020-11-19 19:28:34 +0300

    practical5 - 2 задача выполнена прошу прощения не успеваю, на работе мрак

--- practical5/task2.py
+++ practical5/task2.py
@@ -1,5 +1,8 @@
 # 2. Создать текстовый файл (не программно), сохранить в нем несколько строк,
 # выполнить подсчет количества строк, количества слов в каждой строке.
-f_obj = open('practical4/task2.txt', encoding='utf-8')
-number_of_lines = f_obj.readlines()
-print(f'Количество строк: {len(number_of_lines)}')
+f_obj = open('../practical5/task2.txt', encoding='utf-8')
+lists = f_obj.readlines()
+print(f'Количество строк: {len(lists)}')
+for i, lines in enumerate(lists):
+    count = lines.split()
+    print(f'В строке номер {i+1} слов: {len(count)}')
