a2fa88e2f53c5ff49c361ce34ac0ac35be265825
Artem <molodov@sevavtodor.ru>
2020-11-21 19:01:02 +0300

    practical5 - task 6 проверен работа над ошибками

--- practical5/task6.py
+++ practical5/task6.py
@@ -12,3 +12,29 @@
 # Пример словаря:
 # {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
+
+with open('../practical5/task6.txt', encoding='utf=8') as f:
+    subjects = f.readlines()
+    f.close()
+
+plan = {}
+for subject in subjects:
+    subject = subject.split(' ')
+    subject_name = subject[0]
+    lessons = sum([int(x[:x.find('(')]) for x in subject[1:] if '(' in x])
+    plan[subject_name[:-1]] = lessons
+print(plan)
+
+# образец
+# my_dict = dict()
+# with open('../practical5/task6.txt', encoding='utf=8') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         splitted_line = line.split()
+#         subject = splitted_line[0]
+#         sum_lessons = sum([int(x[:x.find('(')]) for x in splitted_line[1:] if '(' in x])
+#         my_dict[subject[:-1]] = sum_lessons
+# print(my_dict)
+
+# на заметку:
+# повторить срезы и генераторы, свое решение было корявое через перебор и условия
