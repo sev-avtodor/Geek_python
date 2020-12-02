90331e67616faa35c8df5f32edfc23e531cf3225
Artem Work <molodov@sevavtodor.ru>
2020-12-02 14:52:59 +0300

    practical 7 task 2 разобрал

--- practical7/task2.py
+++ practical7/task2.py
@@ -10,50 +10,41 @@
 # Проверить на практике полученные на этом уроке знания:
 # реализовать абстрактные классы для основных классов проекта,
 # проверить на практике работу декоратора @property.
+from abc import ABC, abstractmethod
 
-class Clothes:
+class Clothes(ABC):
 
-    def __init__(self, name):
+    def __init__(self, name, param):
         self.name = name
+        self.param = param
         self.type = ('coat', 'costume')
 
-    # def summ(self):
-    #     sum_consumption = sum(x.consumption for x in self.__dict__.items())
-    #     return sum_consumption
+    @abstractmethod
+    def summ(self):
+        pass
+
 
 
 class Coat(Clothes):
 
-    def __init__(self, name, V):
-        self.V = V
-
-    def tissue_consumption(self):
-        self.__setattr__('consumption', self.V / 6.5 + 0.5)
-        return self.consumption
+    @property
+    def summ(self):
+        return round(self.param / 6.5 + 0.5)
 
 
 class Costume(Clothes):
 
-    def __init__(self, name, H):
-        self.H = H
+    @property
+    def summ(self):
+        return round(2 * self.param + 0.3)
 
-    def tissue_consumption(self):
-        self.__setattr__('consumption', 2 * self.H + 0.3)
-        return self.consumption
-
+# def summarry():
+#     return sum(i.summ for i in Clothes.__dict__.values())
 
 palto1 = Coat('Снежинка', 35)
 palto2 = Coat('Рабочий', 55)
 costume1 = Costume('Деловой', 175)
-print(palto1.tissue_consumption())
-print(palto1.tissue_consumption())
-print(costume1.tissue_consumption())
-
-
-def summ():
-    # sum_consumption = sum(x for x.consumption in Clothes.__dict__.items())
-    print(sum(int((x for x in Clothes.__dict__.values()))))
-    # return sum_consumption
-
-
-print(summ())
+print(palto1.summ)
+print(palto2.summ)
+print(costume1.summ)
+print(f'Общий объем ткани составит: {palto1.summ + palto2.summ + costume1.summ}')
