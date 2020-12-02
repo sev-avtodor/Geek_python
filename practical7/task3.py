70e9dd641df3c9719d61bf045172e737c703b1a8
Artem Work <molodov@sevavtodor.ru>
2020-12-02 16:54:22 +0300

    practical 7 task 3 уточнить вызов переменной и доделать условия

--- practical7/task3.py
+++ practical7/task3.py
@@ -4,6 +4,7 @@
 # сложение (__add__()), вычитание (__sub__()), умножение (__mul__()), деление (__truediv__()).
 #
 # Данные методы должны применяться только к клеткам и выполнять увеличение, уменьшение, умножение и обычное (не целочисленное) деление клеток, соответственно.
+#
 # В методе деления должно осуществляться округление значения до целого числа.
 # Сложение. Объединение двух клеток. При этом число ячеек общей клетки должно равняться сумме ячеек исходных двух клеток.
 # Вычитание. Участвуют две клетки. Операцию необходимо выполнять только если разность количества ячеек двух клеток больше нуля, иначе выводить соответствующее сообщение.
@@ -18,6 +19,44 @@
 # Тогда метод make_order() вернет строку: *****\n*****\n**.
 # Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5.
 # Тогда метод make_order() вернет строку: *****\n*****\n*****.
-class Kletka():
+class Cell():
+    iMenuNumber = 0
 
-    self.__init__(param)
+    def __init__(self, input):
+        self.__class__.iMenuNumber = self.__class__.iMenuNumber + 1
+        self.param = [input, len(str(input))]
+        self.number = self.__class__.iMenuNumber
+
+    def __add__(self, other):
+        return f'Сумма клеток {self.number} и {other.number}: {self.param[0] + other.param[0]}, ячеек: {self.param[1]}'
+
+    def __sub__(self, other):
+        if self.param[0] - other.param[0] > 0:
+            return f'Разница клетки {self.number} и {other.number}: {self.param[0] - other.param[0]}, ячеек: {self.param[1]}'
+        else:
+            return f'Разница клеток {self.number} и {other.number} отрицательна'
+
+    def __mul__(self, other):
+        return f'Произведение клеток {self.number} и {other.number}: {self.param[0] * other.param[0]}, ячеек: {self.param[1] * other.param[1]}'
+
+    def __truediv__(self, other):
+        return f'Произведение клеток {self.number} и {other.number}: {self.param[0] / other.param[0]}, ячеек: {self.param[1] // other.param[1]}'
+
+    def make_order(leight_line, self=None):
+        text = ''
+        # for i in range(iMenuNumber):
+            text += '\n'.join(self.param[0])
+        return text
+
+kletka1 = Cell(15)
+kletka2 = Cell(2)
+kletka3 = Cell(13)
+kletka4 = Cell(4)
+print('Количество клеток:', Cell.iMenuNumber)
+print(kletka1 + kletka2)
+print(kletka4 - kletka3)
+print(kletka3 - kletka2)
+print(kletka1 * kletka4)
+print(kletka1 / kletka4)
+print(Cell.make_order(8))
+
